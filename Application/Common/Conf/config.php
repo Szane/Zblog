5f46809d508dd821ec44d182841af9341360cfe8
Szane <szanezhang@gmail.com>
2015-10-24 11:02:32 +0800

    添加导航栏与模板

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,15 +1,15 @@
 <?php
 return array (
-    'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'qdm190467413.my3w.com',
-    'DB_USER' => 'qdm190467413',
-    'DB_PWD' => 'story261026',
-    'DB_NAME' => 'qdm190467413_db',
-    'DB_PREFIX' => 'ZBLOG_',
-    'DB_PORT' => 3306,
-    'DB_CHARSET' => 'utf8', // 字符集
-    'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
-
-    
+		'DB_TYPE' => 'mysql',
+		'DB_HOST' => 'qdm190467413.my3w.com',
+		'DB_USER' => 'qdm190467413',
+		'DB_PWD' => 'story261026',
+		'DB_NAME' => 'qdm190467413_db',
+		'DB_PREFIX' => 'ZBLOG_',
+		'DB_PORT' => 3306,
+		'DB_CHARSET' => 'utf8', // 字符集
+		'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+		'LAYOUT_ON' => true,
+		'LAYOUT_NAME' => 'Layout/mainlayout' 
 );
 ?>
--- Application/Home/Controller/AccountController.class.php
+++ Application/Home/Controller/AccountController.class.php
@@ -1,27 +1,25 @@
 <?php
-namespace Home \ Controller;
-use Think \ Controller;
+
+namespace Home\Controller;
+
+use Think\Controller;
 
 class AccountController extends Controller {
-    public function index() {
-        $this->display();
-    }
-    public function signin() {
-        $this->display("signin");
-    }
-    public function dosignin() {
-
-    }
-    public function signup() {
-        $this->display('signup');
-    }
-    public function dosignup() {
-        $Guid = new \ Home \ Common \ Util \ Guid();
-        $Account = M('Account');
-        $data['id'] = $Guid->getGuid();
-        $data['username'] = $_POST['text'];
-        $data['password'] = $_POST['pwd'];
-        $Account->add($data);
-    }
+	public function signin() {
+		$this->display ( 'signin' );
+	}
+	public function dosignin() {
+	}
+	public function signup() {
+		$this->display ( 'signup' );
+	}
+	public function dosignup() {
+		$Guid = new \Home\Common\Util\Guid ();
+		$Account = M ( 'Account' );
+		$data ['id'] = $Guid->getGuid ();
+		$data ['username'] = $_POST ['text'];
+		$data ['password'] = $_POST ['pwd'];
+		$Account->add ( $data );
+	}
 }
 ?>
